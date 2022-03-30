fc6b585b222fd2e8076f76290df33f3148080e89
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-03-30 09:00:00 +0530

    Add files via upload

---
+++ Assignment 1/Codes/find.py
@@ -0,0 +1,9 @@
+base_circumference = 132
+height = 25
+
+'''using pi = 22/7'''
+radius = base_circumference/2*7/22
+volume = (22*(radius**2)*height)/7
+
+print(radius,volume)
+print("Theoritical and program values of both radius and volume are equal")
