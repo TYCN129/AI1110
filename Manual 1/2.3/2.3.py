b00958b6c82e52bf1b1cc9eddd9c766cd64385ee
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-01 11:06:08 +0530

    Update 2.3.py

--- Manual 1/2.3/2.3.py
+++ Manual 1/2.3/2.3.py
@@ -10,18 +10,15 @@ X = pd.read_csv("../2.1/gau.dat")
 x = np.linspace(-6,6,pts)
 F = [] 
 for i in range(0,pts):
-	Fcount = np.count_nonzero(X < x[i]) 
+	Fcount = np.size(np.nonzero(X < x[i]))
 	F.append(Fcount/n) 
 
 p = []
 for i in range(0,pts-1):
-	p.append( (F[i+1] - F[i])/ (x[i+1]-x[i]) )
+	p.append( (F[i+1] - F[i])/ (x[i+1]-x[i]))
 	
-	
-
-plt.plot(x.T[0:(pts-1)], p, color="blue" )#plotting the CDF
+plt.plot(x.T[0:(pts-1)], p, color="blue" )
 plt.grid()
-plt.minorticks_on()
 plt.xlabel("x")
 plt.ylabel("$p_X(x)$")
 plt.title("Emperical PDF of X")
