bba320aa0e51cfb9628423aad7217b794a06b68f
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-01 16:50:25 +0530

    Update 2.1.c

--- Manual 1/2.1/2.1.c
+++ Manual 1/2.1/2.1.c
@@ -1,20 +1,9 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
+#include "../coeffs.h"
 
 #define N pow(10,6)
 
 int main() {
-	FILE *pfile = fopen("gau.dat","w+");
-	for(int i = 0; i < N; i++) {
-		double sum = 0;
-		for(int j = 0; j < 12; j++) {
-			sum += ((double)rand()/RAND_MAX);
-		}
-		fprintf(pfile,"%lf\n",sum - 6);
-	}
-
-	fclose(pfile);
-
+        char str[] = "gau.dat";
+        gaussian(str,N);
 return 0;
 }
