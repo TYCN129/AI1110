9b766a32daacfaf3ebb172837c9276403d318003
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-02 11:35:37 +0530

    Update 4.5_2.py

--- Manual 1/4.5/4.5_2.py
+++ Manual 1/4.5/4.5_2.py
@@ -7,24 +7,22 @@ N=10**6
 
 def pdf(x):
         if x <= 0:
-                return 0
+                return 0.0
         elif x > 0 and x <= 1:
                 return x**2/2
         elif x > 1 and x < 2:
                 return 2*x - (x**2/2) - 1
         else:
-                return 1
+                return 1.0
 
-c=[]
+vec_triangular=np.vectorize(pdf)
 F=[]
-for i in range(0,100):
-        c.append(pdf(x[i]))
 
 for i in range(0,100):
         F.append(np.size(np.nonzero(T <  x[i]))/N)
 
-plt.scatter(x,c,label="Theoritical")
-plt.plot(x,F,label="Numerical",color="orange")
+plt.scatter(x,F,label="Theoritical")
+plt.plot(x,vec_triangular(x),label="Numerical",color="orange")
 plt.grid()
 plt.xlabel("x")
 plt.ylabel("$p_T(x)$")
