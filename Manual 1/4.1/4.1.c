ae81d5f9dbb9d2cc6eb63e91ba86c419da2d29af
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-02 09:46:18 +0530

    Update 4.1.c

--- Manual 1/4.1/4.1.c
+++ Manual 1/4.1/4.1.c
@@ -2,20 +2,6 @@
 
 #define N pow(10,6)
 
-void triangular(char *str, int len)
-{
-int i;
-FILE *fp;
-
-fp = fopen(str,"w");
-//Generate numbers
-for (i = 0; i < len; i++)
-{
-fprintf(fp,"%lf\n",(double)rand()/RAND_MAX + (double)rand()/RAND_MAX);
-}
-fclose(fp);
-}
-
 int main() {
 	char str[] = "tri.dat";
 	triangular(str,N);
