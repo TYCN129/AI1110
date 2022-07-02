4478051099659f1f6fd429b2727f8b72e4903831
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-02 11:50:27 +0530

    Update 3.1.py

--- Manual 1/3.1/3.1.py
+++ Manual 1/3.1/3.1.py
@@ -10,17 +10,18 @@ x = np.linspace(-2,10,pts)
 
 F_sim=[]
 for i in range(0,pts):
-	F_sim.append(np.size(np.nonzero(V < x[i]))/N)
+        F_sim.append(np.size(np.nonzero(V < x[i]))/N)
 
-F_theory=[]
-for i in range(0,pts):
-	if x[i] <= 0:
-		F_theory.append(0)
-	else:
-		F_theory.append(1 - np.exp(-x[i]/2))
+def exp_cdf(x):
+        if x <= 0:
+                return 0.0
+        else:
+                return (1 - np.exp(-x/2))
 
-plt.plot(x[0:],F_sim,'o',label="Empirical",color="blue")
-plt.plot(x[0:],F_theory,label="Theoritical",color="orange")
+vec_exp=np.vectorize(exp_cdf)
+
+plt.plot(x,F_sim,'o',label="Empirical",color="blue")
+plt.plot(x,vec_exp(x),label="Theoritical",color="orange")
 plt.grid()
 plt.xlabel("x")
 plt.ylabel("$F_V(x)$")
