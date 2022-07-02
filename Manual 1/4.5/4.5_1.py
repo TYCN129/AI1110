d4b5da438d3faf9f52a4b9cd3b234b757adec04b
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-02 11:12:16 +0530

    Update 4.5_1.py

--- Manual 1/4.5/4.5_1.py
+++ Manual 1/4.5/4.5_1.py
@@ -2,23 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x=np.linspace(-6,6,100)
+T=np.loadtxt("../4.1/tri.dat")
+N=10**6
 
 def pdf(x):
-	if x <= 0:
-		return 0
-	elif x > 0 and x <= 1:
-		return x
-	elif x > 1 and x < 2:
-		return 2 - x
-	else:
-		return 0
+        if x <= 0:
+                return 0
+        elif x > 0 and x <= 1:
+                return x
+        elif x > 1 and x < 2:
+                return 2 - x
+        else:
+                return 0
 
 p=[]
-
 for i in range(0,100):
-	p.append(pdf(x[i]))
+        p.append(pdf(x[i]))
 
-plt.plot(x,p,label="Theoritical")
+F=[]
+pdf=[]
+for i in range(0,100):
+        F.append(np.size(np.nonzero(T <  x[i]))/N)
+for i in range(0,99):
+        pdf.append((F[i+1]-F[i])/(x[i+1]-x[i]))
+
+plt.scatter(x.T,p,label="Theoritical")
+plt.plot(x[0:99],pdf,label="Numerical",color="orange")
 plt.grid()
 plt.xlabel("x")
 plt.ylabel("$p_T(x)$")
