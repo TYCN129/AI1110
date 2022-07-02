ec7b3f0bb6f14e1e87ab2ca6d6ffc57545f5aef1
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-02 11:39:27 +0530

    Update 2.5.py

--- Manual 1/2.5/2.5.py
+++ Manual 1/2.5/2.5.py
@@ -3,20 +3,23 @@ import matplotlib.pyplot as plt
 
 X = np.loadtxt("../2.1/gau.dat")
 N = 10**6
-pts=100
+pts=100 
 
 x = np.linspace(-10,10,pts)
-gauss_pdf = (1/np.sqrt(2*np.pi))*np.exp(-(x**2/2))
+def gauss_pdf(x):
+        return (1/np.sqrt(2*np.pi))*np.exp(-(x**2/2))
 
-F = [] 
+F = []
 for i in range(0,pts):
-	F.append(np.size(np.nonzero(X < x[i]))/N) 
+        F.append(np.size(np.nonzero(X < x[i]))/N) 
 
 p = []
 for i in range(0,pts-1):
-	p.append( (F[i+1] - F[i])/ (x[i+1]-x[i]) )
+        p.append( (F[i+1] - F[i])/ (x[i+1]-x[i]) )
 
-plt.plot(x,gauss_pdf,label = "Theory",color = "orange")
+vec_gauss=np.vectorize(gauss_pdf)
+
+plt.plot(x,vec_gauss(x),label = "Theory",color = "orange")
 plt.scatter(x[0:pts-1],p,label = "Numereical")
 plt.grid()
 plt.legend()
