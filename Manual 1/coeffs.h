aff4c16fddb7b62e4955cfbc526a357889da7258
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-02 09:46:43 +0530

    Update coeffs.h

--- Manual 1/coeffs.h
+++ Manual 1/coeffs.h
@@ -275,3 +275,17 @@ fclose(fp);
 
 }
 //End function for generating Gaussian random numbers
+
+void triangular(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX + (double)rand()/RAND_MAX);
+}
+fclose(fp);
+}
