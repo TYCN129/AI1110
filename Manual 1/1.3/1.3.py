f86413c40233ade7ba384d1f76986bf399c1606f
TYCN129 <102508465+TYCN129@users.noreply.github.com>
2022-07-02 11:54:11 +0530

    Update 1.3.py

--- Manual 1/1.3/1.3.py
+++ Manual 1/1.3/1.3.py
@@ -7,23 +7,26 @@ n = 10**6
 U = pd.read_csv("../1.1/uni.dat")
 
 x = np.linspace(-2,2,pts)
-F = [] 
+F = []
+
+def uniform(x):
+        if(x<0):
+                return 0
+        elif(x>1):
+                return 1
+        else:
+                return x
+
+F_theory=[]
+
+vec_uniform = np.vectorize(uniform)
 
 for i in range(0,pts):
-	F_n = np.count_nonzero(U < x[i]) #checking probability condition
-	F.append(F_n/n) #storing the probability values in a list
-
-F_theory = []
-for i in range(0,pts):
-	if(x[i]<0):
-		F_theory.append(0)  
-	elif(x[i]>1):
-		F_theory.append(1)  
-	else:
-		F_theory.append(x[i]) 
+        F_n = np.count_nonzero(U < x[i]) #checking probability condition
+        F.append(F_n/n) #storing the probability values in a list
 
 plt.scatter(x.T, F, color="blue" )#plotting the CDF
-plt.plot(x.T, F_theory, color="orange" )#plotting the CDF
+plt.plot(x.T, vec_uniform(x), color="orange" )#plotting the CDF
 plt.grid()
 plt.legend()
 plt.xlabel("x")
